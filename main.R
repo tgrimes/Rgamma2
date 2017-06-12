77ac19dc175df5e5dbd4f0d371c1b269c7e616a5
Tyler Grimes <tyler.grimes@ufl.edu>
2017-06-12 15:52:20 -0400

    Removed time keeping

--- main.R
+++ main.R
@@ -13,10 +13,6 @@ m = 10^5              # Number of simulations.
 signf.set = c(0.05)   # Significance levels.
 tail <- "left"        # Hypothesis test (only "left" has been fully implemented).
 
-results <- NULL
-timestart <- NULL
-timeend <- NULL
-
 #------------------------------------------------------------------------
 # Run simulation with the five test statistics. 
 #------------------------------------------------------------------------
@@ -28,9 +24,7 @@ delta.set <- c(1, 2, 3, 4)
 #       in the run.simulation() function in the simulation.R script.
 for(n in n.set) {
   for(significance.level in signf.set) {
-    timestart <- format(Sys.time(), "%Y_%m_%d_%H.%M.%S")
     results <- run.simulation(n, m, significance.level, ht.list, delta.set, tail)
-    timeend <- format(Sys.time(), "%Y_%m_%d_%H.%M.%S")
     
     #Edit test names. These are used in graphs and tables.
     for(i in 1:length(results)) {
@@ -46,8 +40,6 @@ for(n in n.set) {
     dir.create(dir.save)
     save.image(file = paste(dir.save, "/env", sep = ""))
     
-    source("script_export_figures_tables.R")
-    
-    print(paste("n =", n, "- start:", timestart, "     end:", timeend))
+    source("script_export_figures_tables.R")    
   }
 }
